9a6ece0b629f29f034a20d48ebecbc2eca65f61b
Owethu99Twala <owethutwala99@gmail.com>
2020-07-14 16:42:23 +0200

    updated commit

--- calc.js
+++ calc.js
@@ -1,23 +1,23 @@
 
 
-//   var inputLabel = document.getElementById('inputLabel');
+  var inputLabel = document.getElementById('inputLabel');
 
-//   function pushBtn(obj){
-//       var pushed = obj.innerHTML;
-//       if(pushed == "="){
-//           inputLabel.innerHTML = eval(inputLabel.innerHTML);
-//       }
+  function pushBtn(obj){
+      var pushed = obj.innerHTML;
+      if(pushed == "="){
+          inputLabel.innerHTML = eval(inputLabel.innerHTML);
+      }
 
-//       else if(pushed == 'AC'){
-//           inputLabel.innerHTML = '0';
-//       }
+      else if(pushed == 'AC'){
+          inputLabel.innerHTML = '0';
+      }
 
-//       else{
-//           if(inputLabel.innerHTML == '0'){
-//               inputLabel.innerHTML = pushed;
-//           }
-//           else{
-//               inputLabel.innerHTML += pushed;
-//           }
-//       }
-//   }
+      else{
+          if(inputLabel.innerHTML == '0'){
+              inputLabel.innerHTML = pushed;
+          }
+          else{
+              inputLabel.innerHTML += pushed;
+          }
+      }
+  }
